7502ed47555684153bea943d6bf08fc4a988cc89
crims1n <nar@mate.com>
2023-01-18 21:37:24 -0500

    finished main skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     extend: {}
   },
   daisyui: {
-    themes: ["black"],
+    themes: [{
+      drewcula: {
+        "primary": "#DC143C",
+        "secondary": "#BD93F9",
+        "accent": "#FFB86C",
+        "neutral": "#414558",
+        "base-100": "#282A36",
+        "info": "#8BE9FD",
+        "success": "#50FA7B",
+        "warning": "#F1FA8C",
+        "error": "#FF5555",
+      }
+    }],
   },
   plugins: [require("daisyui")],
 };
